3f5b21793446aaff5a2c543cec05e9621a010c91
aman <aman.aggarwal006@gmail.com>
2019-05-20 20:03:30 +0530

    Added security using AuthManagerBuilder and made repository for accessing database

--- src/main/java/net/guides/todo/todomanagement/repository/TodoRepository.java
+++ src/main/java/net/guides/todo/todomanagement/repository/TodoRepository.java
@@ -3,6 +3,10 @@ package net.guides.todo.todomanagement.repository;
 import net.guides.todo.todomanagement.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findByUserName(String userName);
 
 }
---
+++ src/main/java/net/guides/todo/todomanagement/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package net.guides.todo.todomanagement.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("admin").password("admin").roles("USER", "ADMIN");
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeRequests().antMatchers("/login","h2-console/**").permitAll()
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and().formLogin();
+
+        //This blocks the cross site reference forgery
+        httpSecurity.csrf().disable();
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
